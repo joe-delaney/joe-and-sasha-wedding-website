c5f6e276534eacdabe51206d46f8bd2a2da70213
Joe Delaney <joe@Joes-MBP.lan>
2025-03-20 16:24:05 -0400

    update registry language

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -4,7 +4,7 @@ const Registry = () => {
     return (
         <div className="registry container mx-auto flex flex-col bg-light-blue-100">
             <div className="registry-text flex flex-col gap-1 font-anton text-light-blue-500">
-                <span>Your presence at our wedding is the best present! There is absolutely no requirement to give us a gift, but we have put together this registry to offer suggestions. Please click the button below to view our registry.</span>
+                <span>Your presence is our present! Getting us a gift is by no means an expectation. If you would like to, we have put together this page to help offer some suggestions of things that would be meaningful to us. We also understand how lucky and privileged we are, and therefore, we have included meaningful charities to us on our registry if you would like to donate instead of a gift.</span>
             </div>
             <a href="https://www.zola.com/registry/delaneymillerwedding" target="_blank" rel="noreferrer" className="registry-button flex-center border w-64 h-8 rounded-lg bg-light-blue-500 text-light-blue-100">Joe & Sasha's Registry</a>
         </div>
